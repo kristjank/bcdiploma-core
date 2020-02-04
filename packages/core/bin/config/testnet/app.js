92374aa6c1cf085cb01557d96aa6a9ba67e5c3e5
Kristjan Kosic - Chris <chris@ark.io>
2020-02-04 07:59:24 +0100

    chore: add plugins to separate process registrations.

--- packages/core/bin/config/testnet/app.js
+++ packages/core/bin/config/testnet/app.js
@@ -3,37 +3,52 @@ module.exports = {
         core: {
             run: {
                 plugins: {
-                    include: ["@arkecosystem/core-magistrate-transactions"],
+                    include: [
+                        "@arkecosystem/core-magistrate-transactions",
+                        "@bcdiploma/certificate-manager"
+                    ],
                 },
             },
         },
         relay: {
             run: {
                 plugins: {
-                    include: ["@arkecosystem/core-magistrate-transactions"],
+                    include: [
+                        "@arkecosystem/core-magistrate-transactions",
+                        "@bcdiploma/certificate-manager"
+                    ],
                 },
             },
         },
         forger: {
             run: {
                 plugins: {
-                    include: ["@arkecosystem/core-magistrate-transactions"],
+                    include: [
+                        "@arkecosystem/core-magistrate-transactions",
+                        "@bcdiploma/certificate-manager"
+                    ],
                 },
             },
         },
         chain: {
             run: {
                 plugins: {
-                    include: ["@arkecosystem/core-magistrate-transactions"],
+                    include: [
+                        "@arkecosystem/core-magistrate-transactions",
+                        "@bcdiploma/certificate-manager"
+                    ],
                 },
             },
         },
         snapshot: {
             run: {
                 plugins: {
-                    include: ["@arkecosystem/core-magistrate-transactions"],
+                    include: [
+                        "@arkecosystem/core-magistrate-transactions",
+                        "@bcdiploma/certificate-manager"
+                    ],
                 },
             },
         },
     },
-}
+};
